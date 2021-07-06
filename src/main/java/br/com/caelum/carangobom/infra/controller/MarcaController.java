5d4d07416182c365b9fca9c6e4b893b206f9572a
Willyan Antunes <willyanantunes@MacBook-Pro-de-Willyan.local>
2021-07-06 16:03:55 -0300

    fixing type errors

--- src/main/java/br/com/caelum/carangobom/infra/controller/MarcaController.java
+++ src/main/java/br/com/caelum/carangobom/infra/controller/MarcaController.java
@@ -1,5 +1,7 @@
-package br.com.caelum.carangobom.marca;
+package br.com.caelum.carangobom.infra.controller;
 
+import br.com.caelum.carangobom.domain.entity.Marca;
+import br.com.caelum.carangobom.domain.repository.MarcaRepository;
 import br.com.caelum.carangobom.validacao.ErroDeParametroOutputDto;
 import br.com.caelum.carangobom.validacao.ListaDeErrosOutputDto;
 import org.springframework.beans.factory.annotation.Autowired;
