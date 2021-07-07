08e71fd707a0b572c6168867ca778d78cfe9e2b3
Willyan Antunes <willyanantunes@MacBook-Pro-de-Willyan.local>
2021-07-07 11:12:52 -0300

    removing business logic from controller

---
+++ src/main/java/br/com/caelum/carangobom/infra/controller/request/CreateMarcaRequest.java
@@ -0,0 +1,19 @@
+package br.com.caelum.carangobom.infra.controller.request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import br.com.caelum.carangobom.infra.jpa.entity.MarcaJpa;
+import lombok.Getter;
+
+public class CreateMarcaRequest{
+
+	@NotBlank 
+	@Size(min = 2, message = "Deve ter {min} ou mais caracteres.") 
+	@Getter
+	private String nome;
+
+	public MarcaJpa toMarcaJpa(){
+		return new MarcaJpa(nome);
+	}
+}
