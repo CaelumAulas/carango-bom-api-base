1deb17ca4ab5491e683af1123a48908ccd6730ee
lamatheus.g <matheus.morillo2000@gmail.com>
2021-07-08 11:52:12 -0300

    Add bean validation to CreateUserRequest

--- src/main/java/br/com/caelum/carangobom/infra/controller/request/CreateUserRequest.java
+++ src/main/java/br/com/caelum/carangobom/infra/controller/request/CreateUserRequest.java
@@ -4,9 +4,15 @@ import br.com.caelum.carangobom.domain.entity.User;
 import br.com.caelum.carangobom.infra.jpa.entity.UserJpa;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class CreateUserRequest {
+
+    @NotNull @NotBlank
     private String username;
+    @NotNull @NotBlank
     private String password;
 
     public User toUser() {
