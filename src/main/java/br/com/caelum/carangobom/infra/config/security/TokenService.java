c793984bcf8c7aafee3183f5851513b70e33a356
lamatheus.g <matheus.morillo2000@gmail.com>
2021-07-14 15:07:04 -0300

    refactor jwt metadata names

--- src/main/java/br/com/caelum/carangobom/infra/config/security/TokenService.java
+++ src/main/java/br/com/caelum/carangobom/infra/config/security/TokenService.java
@@ -13,9 +13,9 @@ import java.util.Date;
 @Service
 public class TokenService {
 
-    @Value("${forum.jwt.expiration}")
+    @Value("${carangobom.jwt.expiration}")
     private String expiration;
-    @Value("${forum.jwt.secret}")
+    @Value("${carangobom.jwt.secret}")
     private String secret;
 
     public String generateToken(Authentication auth) {
