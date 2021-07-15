c47f9d61183bd1be83bc3b3c4c9bbd90d5314cba
Willyan Antunes <willyanantunes@MacBook-Pro-de-Willyan.local>
2021-07-15 10:16:43 -0300

    added get all paginated method in repository

--- src/main/java/br/com/caelum/carangobom/domain/repository/VehicleRepository.java
+++ src/main/java/br/com/caelum/carangobom/domain/repository/VehicleRepository.java
@@ -1,10 +1,14 @@
 package br.com.caelum.carangobom.domain.repository;
 
 import br.com.caelum.carangobom.domain.entity.Vehicle;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface VehicleRepository {
     Vehicle save(Vehicle vehicle);
     Optional<Vehicle> findById(Long vehicleId);
+    Page<Vehicle> getAll(Pageable pageable);
 }
--- src/main/java/br/com/caelum/carangobom/infra/jpa/repository/VehicleRepositoryJpa.java
+++ src/main/java/br/com/caelum/carangobom/infra/jpa/repository/VehicleRepositoryJpa.java
@@ -4,10 +4,15 @@ import br.com.caelum.carangobom.domain.entity.Vehicle;
 import br.com.caelum.carangobom.domain.repository.VehicleRepository;
 import br.com.caelum.carangobom.infra.jpa.entity.VehicleJpa;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -40,5 +45,20 @@ public class VehicleRepositoryJpa implements VehicleRepository {
     public Optional<Vehicle> findById(Long vehicleId) {
         VehicleJpa vehicleJpa = this.entityManager.find(VehicleJpa.class, vehicleId);
         return Optional.ofNullable(vehicleJpa);
+    }
+
+    @Override
+    public Page<Vehicle> getAll(Pageable pageable) {
+        List vehicleList;
+        if(pageable.isPaged()){
+            Query query = entityManager.createQuery("From vehicle");
+            query.setFirstResult((int) pageable.getOffset());
+            query.setMaxResults(pageable.getPageSize());
+            vehicleList = query.getResultList();
+        }else{
+            vehicleList = entityManager.createQuery("Select v from vehicle v",VehicleJpa.class).getResultList();
+        }
+        Long countResult = entityManager.createQuery("Select count(v.id) From vehicle v", Long.class).getSingleResult();
+        return new PageImpl(vehicleList, pageable ,countResult);
     }
 }
