9c525556dfe0c666d906c8dddf5f15e9f5ba4a3b
Willyan Antunes <willyanantunes@MacBook-Pro-de-Willyan.local>
2021-07-15 15:18:49 -0300

    fixed code smells issues

--- src/main/java/br/com/caelum/carangobom/domain/service/VehicleService.java
+++ src/main/java/br/com/caelum/carangobom/domain/service/VehicleService.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-import java.util.OptionalInt;
 
 @Service
 public class VehicleService {
