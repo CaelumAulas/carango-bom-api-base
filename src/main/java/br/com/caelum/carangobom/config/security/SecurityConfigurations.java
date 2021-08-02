bc817bf2c2f2c65fc0b7a677c19c7d25e11d4657
José Victor Viriato <victor.viriato@thoughtworks.com>
2021-08-02 15:23:45 -0300

    fix: Remove imports

--- src/main/java/br/com/caelum/carangobom/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/caelum/carangobom/config/security/SecurityConfigurations.java
@@ -2,9 +2,7 @@ package br.com.caelum.carangobom.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
